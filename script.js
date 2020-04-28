fc13568d15e27dc360e398ce0bf9ac8a6a0543f8
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-04-28 18:28:46 -0500

    inital commit, formatted html with rows for 9-5

---
+++ script.js
@@ -0,0 +1,6 @@
+// variables for moment js
+pText = moment().format("dddd MMM Do YYYY")
+// variables for selecting elements
+var currentDay = document.getElementById("currentDay");
+
+currentDay.textContent = pText
