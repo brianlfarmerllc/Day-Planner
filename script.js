9f2ad2c5abadd1480ad33f986a2edc11228cc259
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-04-28 19:09:15 -0500

    installed moment.js and verified function in current day

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-// variables for moment js
-pText = moment().format("dddd MMM Do YYYY")
-// variables for selecting elements
 var currentDay = document.getElementById("currentDay");
-
-currentDay.textContent = pText
+currentDay.textContent = moment().format("dddd MMM Do YYYY");
