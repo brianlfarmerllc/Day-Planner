6354f6978c2ab64c4f656fd450ee128d88ff8864
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-04-30 13:03:13 -0500

    added confirm to clear daily schedule button

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Day-Planner
 
-![Day Planner Quick View](assets/Day-Planner-Img.png)
+![Day Planner Quick View](Assets/Day-Planner-Img.png)
 
 ## Summary 
 
-This application is a simple calander that allows the user to save tasks and schedule events for each hour of the day. This application run's in the browser. Feature's dynamically updated HTML and CSS powered by jQuery. In addition the app display's standard business hours (9 a.m. to 5 p.m.) along the left hand side of each input row. Depending on the time of day, the schedule text imput areas are updated with their color indicating to the user if items are in the past, present or future.
+This application is a simple calander that run's in the browser. The application allows the user to save tasks in local storage so scheduled events for each hour time block will be displayed anytime the application is open. The application feature's dynamically updated HTML and CSS powered by jQuery. The app works off stanard business hours (9 a.m. to 5 p.m.) which are displayed along the left hand side of each input row. Depending on the time of day, the schedule text imput areas are updated with their color indicating to the user if items are in the past, present or future. After the day has ended the user can easily reset the application with the "Clear Daily Schedule" button at the top of the page. For best use save the url to a bookmark tab to access in browser throughout the day. 
 
 ### Technologies Used
 - jQuery - Used for event listeners of parent and childeren elements as well as to store and recall those varible in local storage.
--- script.js
+++ script.js
@@ -7,51 +7,57 @@ var displayDate = document.getElementById("currentDay");
 // sets day and date at the top of the work day scheduler
 displayDate.textContent = currentDayDate
 
-$(document).ready(function(){
+$(document).ready(function () {
     //Adds a function at the begining of the page to clear the list     
-    $(".clearBtn").on("click", function(event){
-        localStorage.clear();
-        location.reload();
+    $(".clearBtn").on("click", function (event) {
+        var youSure = confirm("Are you sure you want to clear schedule")
+        if (youSure === true) {
+            localStorage.clear();
+            location.reload();
+        } else {
+            return
+        }
+
     });
     //controls the save button functions to save the content in the class = description as well as the save buttons parrent ID 
-    $(".saveBtn").on("click", function(){
+    $(".saveBtn").on("click", function () {
         var time = $(this).parent().attr("id");
         var activity = $(this).siblings(".description").val();
         localStorage.setItem(time, activity);
         console.log(time, activity);
     });
     // looks at the current hr using moment and compares it to the id hour to set the color as past future and present
-    function colorChange(event){
+    function colorChange(event) {
         var timeBlock = moment().hour();
 
-        $(".time-block").each(function() {
-            var blockHour =  parseInt($(this).attr("id"));
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id"));
             console.log(blockHour);
 
             if (blockHour < timeBlock) {
                 $(this).addClass("past");
             }
-            else if (blockHour === timeBlock){
+            else if (blockHour === timeBlock) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
-            }else {
+            } else {
                 $(this).removeClass("past");
                 $(this).removeClass("present");
                 $(this).addClass("future");
             }
         });
     }
-   colorChange();
+    colorChange();
     // gets info for id and text content sections. 
-   $("#9 .description").val(localStorage.getItem("9"));
-   $("#10 .description").val(localStorage.getItem("10"));
-   $("#11 .description").val(localStorage.getItem("11"));
-   $("#12 .description").val(localStorage.getItem("12"));
-   $("#13 .description").val(localStorage.getItem("13"));
-   $("#14 .description").val(localStorage.getItem("14"));
-   $("#15 .description").val(localStorage.getItem("15"));
-   $("#16 .description").val(localStorage.getItem("16"));
-   $("#17 .description").val(localStorage.getItem("17"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
 });
 
