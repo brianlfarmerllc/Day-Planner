3bd51cbde8d2ee8621e39d3f8c6dac583a3422c2
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-04-30 08:44:17 -0500

    added clear button to clear local storage and refresh the page

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Moment variables 
-var currentTime = moment().format("h:mm:ss a")
-var currentDay = moment().format("dddd")
 var currentDayDate = moment().format("dddd MMM Do YYYY");
 
 // variables for selection in html 
@@ -10,6 +8,11 @@ var displayDate = document.getElementById("currentDay");
 displayDate.textContent = currentDayDate
 
 $(document).ready(function(){
+    $(".clearBtn").on("click", function(event){
+        localStorage.clear();
+        location.reload();
+    });
+    
     $(".saveBtn").on("click", function(){
         var time = $(this).parent().attr("id");
         var activity = $(this).siblings(".description").val();
@@ -20,7 +23,8 @@ $(document).ready(function(){
         var timeBlock = moment().hour();
 
         $(".time-block").each(function() {
-            var blockHour =  parseInt($(this).attr("id").split("-")[1]);
+            var blockHour =  parseInt($(this).attr("id"));
+            console.log(blockHour);
 
             if (blockHour < timeBlock) {
                 $(this).addClass("past");
@@ -37,15 +41,15 @@ $(document).ready(function(){
     }
    colorChange();
    
-   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+   $("#9 .description").val(localStorage.getItem("9"));
+   $("#10 .description").val(localStorage.getItem("10"));
+   $("#11 .description").val(localStorage.getItem("11"));
+   $("#12 .description").val(localStorage.getItem("12"));
+   $("#13 .description").val(localStorage.getItem("13"));
+   $("#14 .description").val(localStorage.getItem("14"));
+   $("#15 .description").val(localStorage.getItem("15"));
+   $("#16 .description").val(localStorage.getItem("16"));
+   $("#17 .description").val(localStorage.getItem("17"));
 
 });
 
